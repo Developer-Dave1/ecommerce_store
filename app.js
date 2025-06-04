7efe5ee4dc35903435831a5f325feceba1515b14
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2025-06-03 20:23:47 -0400

    added CSS and updated pug files

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = express.Router();
 const app = express();
 const path = require('path');
 const Product = require('./lib/products.js');
@@ -8,24 +7,39 @@ const { Client } = require('pg');
 const client = new Client({ database: 'ecommerce' });
 client.connect();
 
-
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');  
+app.set('views', path.join(__dirname, 'views'));  
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', async (req, res) => {
+
+app.get('/', (req, res) => {
   try {
-    const products = await Product.getAllProducts(client); // or getAllProducts(client)
+    res.render('index');
+  } catch (error) {
+    console.log(`There was an error reaching the index page.`)
+    throw error;
+  }
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.getAllProducts(client);
+    console.log('Products fetched:', products);
     res.render('products', { products });
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching products:', error);
     res.status(500).send('Error loading products');
   }
 });
 
+app.use((req, res) => {
+  res.status(404).render('not-found'); 
+});
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
+
+
