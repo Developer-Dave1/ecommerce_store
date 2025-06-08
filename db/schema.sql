7b10776f8caaf17cd6ae75cf115cff16e30d2af5
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2025-06-08 17:28:54 -0400

    Save changes before switching branches

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,6 @@ CREATE TABLE users (
     password VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE cart (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL UNIQUE REFERENCES users(id)
-);
-
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     product_name VARCHAR(50) NOT NULL UNIQUE,
@@ -18,8 +13,7 @@ CREATE TABLE products (
 );
 
 CREATE TABLE cart_products (
-    cart_id INTEGER NOT NULL REFERENCES cart(id) ON DELETE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     product_id INTEGER NOT NULL REFERENCES products(id) ON DELETE CASCADE,
-    quantity INTEGER DEFAULT 1 CHECK(quantity > 0),
-    PRIMARY KEY(cart_id, product_id)
+    quantity INTEGER DEFAULT 1 CHECK(quantity > 0)
 );
